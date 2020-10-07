9adf83fe9d89eee113a8ec16705a40a693b19500
chchiu <chchiu@pass-2.ece.utah.edu>
2020-10-07 15:24:28 -0600

    retab

--- ComputeCpp-CE-2.2.0-x86_64-linux-gnu/examples/simple-vector-add.cpp
+++ ComputeCpp-CE-2.2.0-x86_64-linux-gnu/examples/simple-vector-add.cpp
@@ -41,54 +41,55 @@ template <typename T>
 class SimpleVadd;
 
 template <typename T, size_t N>
-void simple_vadd(const std::array<T, N>& VA, const std::array<T, N>& VB,
-                 std::array<T, N>& VC) {
-  cl::sycl::queue deviceQueue;
+void simple_vadd(const std::array<T, N>& VA, const std::array<T, N>& VB, std::array<T, N>& VC){
+    cl::sycl::queue deviceQueue;
 
-  auto device = deviceQueue.get_device();
-  auto deviceName = device.get_info<cl::sycl::info::device::name>();
-  std::cout << " Device Name: " << deviceName << std::endl;
-  auto platformName = device.get_platform().get_info<cl::sycl::info::platform::name>();
-  std::cout << " Platform Name " << platformName << std::endl;
+    auto device = deviceQueue.get_device();
+    auto deviceName = device.get_info<cl::sycl::info::device::name>();
+    std::cout << " Device Name: " << deviceName << std::endl;
+    auto platformName = device.get_platform().get_info<cl::sycl::info::platform::name>();
+    std::cout << " Platform Name " << platformName << std::endl;
 
-  cl::sycl::range<1> numOfItems{N};
-  cl::sycl::buffer<T, 1> bufferA(VA.data(), numOfItems);
-  cl::sycl::buffer<T, 1> bufferB(VB.data(), numOfItems);
-  cl::sycl::buffer<T, 1> bufferC(VC.data(), numOfItems);
+    cl::sycl::range<1> numOfItems{N};
+    cl::sycl::buffer<T, 1> bufferA(VA.data(), numOfItems);
+    cl::sycl::buffer<T, 1> bufferB(VB.data(), numOfItems);
+    cl::sycl::buffer<T, 1> bufferC(VC.data(), numOfItems);
 
-  deviceQueue.submit([&](cl::sycl::handler& cgh) {
-    auto accessorA = bufferA.template get_access<sycl_read>(cgh);
-    auto accessorB = bufferB.template get_access<sycl_read>(cgh);
-    auto accessorC = bufferC.template get_access<sycl_write>(cgh);
+    deviceQueue.submit([&](cl::sycl::handler& cgh){
+        auto accessorA = bufferA.template get_access<sycl_read>(cgh);
+        auto accessorB = bufferB.template get_access<sycl_read>(cgh);
+        auto accessorC = bufferC.template get_access<sycl_write>(cgh);
 
-    auto kern = [=](cl::sycl::id<1> wiID) {
-      accessorC[wiID] = accessorA[wiID] + accessorB[wiID];
-    };
-    cgh.parallel_for<class SimpleVadd<T>>(numOfItems, kern);
-  });
+        auto kern = [=](cl::sycl::id<1> wiID){
+            accessorC[wiID] = accessorA[wiID] + accessorB[wiID];
+        };
+    
+        cgh.parallel_for<class SimpleVadd<T>>(numOfItems, kern);
+    });
 }
 
-int main() {
-  std::cout << "\n\n\n\nright here\n\n\n\n\n";
-  const size_t array_size = 4;
-  std::array<cl::sycl::cl_int, array_size> A = {{1, 2, 3, 4}},
-                                           B = {{1, 2, 3, 4}}, C;
-  std::array<cl::sycl::cl_float, array_size> D = {{1.f, 2.f, 3.f, 4.f}},
-                                             E = {{1.f, 2.f, 3.f, 4.f}}, F;
-  simple_vadd(A, B, C);
-  simple_vadd(D, E, F);
-  for (unsigned int i = 0; i < array_size; i++) {
-    if (C[i] != A[i] + B[i]) {
-      std::cout << "The results are incorrect (element " << i << " is " << C[i]
-                << "!\n";
-      return 1;
-    }
-    if (F[i] != D[i] + E[i]) {
-      std::cout << "The results are incorrect (element " << i << " is " << F[i]
-                << "!\n";
-      return 1;
-    }
-  }
-  std::cout << "The results are correct!\n";
-  return 0;
+
+int main(){
+    const size_t array_size = 4;
+    
+    std::array<cl::sycl::cl_int, array_size> A = {{1, 2, 3, 4}}, B = {{1, 2, 3, 4}}, C;
+    std::array<cl::sycl::cl_float, array_size> D = {{1.f, 2.f, 3.f, 4.f}}, E = {{1.f, 2.f, 3.f, 4.f}}, F;
+    
+    simple_vadd(A, B, C);
+    simple_vadd(D, E, F);
+  
+    for(unsigned int i = 0; i < array_size; i++){
+        if(C[i] != A[i] + B[i]){
+            std::cout << "The results are incorrect (element " << i << " is " << C[i] << "!\n";
+            return 1;
+        }
+
+        if(F[i] != D[i] + E[i]){
+            std::cout << "The results are incorrect (element " << i << " is " << F[i] << "!\n";
+            return 1;
+        }
+    }  
+
+    std::cout << "The results are correct!\n";
+    return 0;
 }
