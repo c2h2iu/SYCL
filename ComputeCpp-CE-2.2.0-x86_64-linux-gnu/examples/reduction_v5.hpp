f9a35202a57403cb89073b35e6920cf04f733fec
chchiu <chchiu@pass-2.ece.utah.edu>
2020-10-08 15:38:03 -0600

    5th reduction

---
+++ ComputeCpp-CE-2.2.0-x86_64-linux-gnu/examples/reduction_v5.hpp
@@ -0,0 +1,166 @@
+/***************************************************************************
+ *
+ *  Copyright (C) 2016 Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Codeplay's ComputeCpp SDK
+ *
+ *  reduction.cpp
+ *
+ *  Description:
+ *    Example of a reduction operation in SYCL.
+ *
+ **************************************************************************/
+
+#include <CL/sycl.hpp>
+#include <iostream>
+#include <vector>
+
+
+
+namespace chiu{
+
+    template<typename T>
+    class sycl_reduction;
+
+
+    template <typename T, typename I, typename C>
+    T sycl_reduce(const std::vector<T>& v, I& init, C bop){
+        T retVal;
+
+        {
+            cl::sycl::queue q([=](cl::sycl::exception_list eL){
+                try{
+                    for(auto& e : eL)    std::rethrow_exception(e);
+                } catch (cl::sycl::exception ex){
+                    std::cout << " There is an exception in the reduction kernel\n";
+                    std::cout << ex.what() << '\n';
+                }
+            });
+
+            /* Output device and platform information. */
+            auto device = q.get_device();
+        
+            auto deviceName = device.get_info<cl::sycl::info::device::name>();
+            std::cout << "Device Name: " << deviceName << '\n';
+        
+            auto platformName = device.get_platform().get_info<cl::sycl::info::platform::name>();
+            std::cout << "Platform Name " << platformName << '\n';
+
+            /* The buffer is used to initialise the data on the device, but we don't
+             * want to copy back and trash it. buffer::set_final_data() tells the
+             * SYCL runtime where to put the data when the buffer is destroyed; nullptr
+             * indicates not to copy back. The vector's length is used as the global
+             * work size (unless that is too large). */
+            cl::sycl::buffer<int, 1> bufI(v.data(), cl::sycl::range<1>(v.size()));
+            bufI.set_final_data(nullptr);
+        
+            size_t local = std::min(v.size(), device.get_info<cl::sycl::info::device::max_work_group_size>());
+            size_t length = v.size();
+
+            if(length == 0)    return init;
+            if(length == 1)    return bop(init, v[0]);
+
+            {
+                /* Each iteration of the do loop applies one level of reduction until
+                 * the input is of length 1 (i.e. the reduction is complete). */
+                do{
+                    auto f = [length, local, &bufI, bop](cl::sycl::handler& h) mutable{
+                        cl::sycl::nd_range<1> r{ cl::sycl::range<1>{ std::max(length, local) }, cl::sycl::range<1>{ std::min(length, local) } };
+
+                        /* Two accessors are used: one to the buffer that is being reduced,
+                         * and a second to local memory, used to store intermediate data. */
+                        auto aI = bufI.get_access<cl::sycl::access::mode::read_write>(h);
+                        cl::sycl::accessor<T, 1, cl::sycl::access::mode::read_write, 
+                                                 cl::sycl::access::target::local> scratch(cl::sycl::range<1>(local), h);
+
+                        cl::sycl::stream os(1024, 32, h);
+                        /* The parallel_for invocation chosen is the variant with an nd_item
+                         * parameter, since the code requires barriers for correctness. */
+                        h.parallel_for<sycl_reduction<T>>(r, [aI, scratch, local, length, bop, os](cl::sycl::nd_item<1> id){
+                            //size_t globalid = id.get_global_id(0);
+                            size_t localid = id.get_local_id(0);
+                     
+                            size_t iid = id.get_local_id(0) + id.get_group(0) * (id.get_local_range(0));
+                            os << "iid = " << iid << '\n'; 
+                            /* All threads collectively read from global memory into local.
+                             * The barrier ensures all threads' IO is resolved before
+                             * execution continues (strictly speaking, all threads within
+                             * a single work-group - there is no co-ordination between
+                             * work-groups, only work-items). */
+                            if(iid < length){
+                                if(id.get_group_range(0) > 1){
+                                    if(iid + id.get_local_range(0) < length)    scratch[localid] = bop(aI[iid], aI[iid + id.get_local_range(0)]);
+                                    else                                        scratch[localid] = aI[iid];
+                                }
+                                else    scratch[localid] = aI[iid];
+                            }
+                            id.barrier(cl::sycl::access::fence_space::local_space);
+
+                            /* Apply the reduction operation between the current local
+                             * id and the one on the other half of the vector. */
+                            for(size_t offset = id.get_local_range(0) / 2; offset > 32; offset /= 2){
+                                if(localid < offset)    scratch[localid] = bop(scratch[localid], scratch[localid + offset]);
+                                id.barrier(cl::sycl::access::fence_space::local_space);
+                            }
+                 
+                            if(localid < 32){
+                                if((length > 32) && (localid + 32 < length))    scratch[localid] = bop(scratch[localid], scratch[localid + 32]);
+                                id.barrier(cl::sycl::access::fence_space::local_space);
+                                
+                                if((length > 16) && (localid + 16 < length))    scratch[localid] = bop(scratch[localid], scratch[localid + 16]);
+                                id.barrier(cl::sycl::access::fence_space::local_space);
+
+                                if((length >  8) && (localid + 8  < length))    scratch[localid] = bop(scratch[localid], scratch[localid + 8]);
+                                id.barrier(cl::sycl::access::fence_space::local_space);
+                                
+                                if((length >  4) && (localid + 4  < length))    scratch[localid] = bop(scratch[localid], scratch[localid + 4]);
+                                id.barrier(cl::sycl::access::fence_space::local_space);
+
+                                if((length >  2) && (localid + 2  < length))    scratch[localid] = bop(scratch[localid], scratch[localid + 2]);
+                                id.barrier(cl::sycl::access::fence_space::local_space);
+
+                                if((length >  1) && (localid + 1  < length))    scratch[localid] = bop(scratch[localid], scratch[localid + 1]);
+                                id.barrier(cl::sycl::access::fence_space::local_space);
+                            }
+                            /* The final result will be stored in local id 0. */
+                            if(localid == 0)    aI[id.get_group(0)] = scratch[localid];
+                        });
+                    };
+
+                    q.submit(f);
+
+                    /* At this point, you could queue::wait_and_throw() to ensure that
+                     * errors are caught quickly. However, this would likely impact
+                     * performance negatively. */                
+                    length = length / local;
+                    length = length / 2;
+
+                } while(length > 1);
+            }
+
+            {
+                /* It is always sensible to wrap host accessors in their own scope as
+                 * kernels using the buffers they access are blocked for the length
+                 * of the accessor's lifetime. */
+                auto hI = bufI.template get_access<cl::sycl::access::mode::read>();
+                retVal = bop(init, hI[0]);
+            }
+        }
+
+        return retVal;
+    }
+}
